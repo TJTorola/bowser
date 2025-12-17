d773962950b4ad79f859c2a55296d7103e95cefb
Tyler Torola <Tyler@TJT.Codes>
2025-12-17 18:14:52 -0500

    Make window internals private

--- src/Window.ts
+++ src/Window.ts
@@ -3,53 +3,53 @@ import NavBar from './NavBar/index.ts';
 import Browser from './Browser/index.ts';
 
 export default class Window {
-  base: BaseWindow;
-  browser: Browser;
-  navBar: NavBar;
+  #base: BaseWindow;
+  #browser: Browser;
+  #navBar: NavBar;
 
   constructor(location: string) {
-    this.base = new BaseWindow({
+    this.#base = new BaseWindow({
       titleBarStyle: 'hidden',
       width: 800,
       height: 600,
       darkTheme: true,
     });
 
-    this.browser = new Browser();
-    this.navBar = new NavBar();
-    this.browser.onUrlChange(this.navBar.updateUrl);
+    this.#browser = new Browser();
+    this.#navBar = new NavBar();
+    this.#browser.onUrlChange(this.#navBar.updateUrl);
 
-    this.base.contentView.addChildView(this.browser.view);
-    this.base.contentView.addChildView(this.navBar.view);
+    this.#base.contentView.addChildView(this.#browser.view);
+    this.#base.contentView.addChildView(this.#navBar.view);
     this.setBounds();
 
-    this.base.on('resize', () => this.setBounds());
+    this.#base.on('resize', () => this.setBounds());
 
     this.loadLocation(location);
   }
 
   get webContents() {
-    return this.browser.view.webContents;
+    return this.#browser.view.webContents;
   }
 
   setWindowOpenHandler = (callback: (url: string) => Electron.WebContents) => {
-    this.browser.setWindowOpenHandler(callback);
+    this.#browser.setWindowOpenHandler(callback);
   };
 
   onFocus = (callback: () => unknown) => {
-    this.base.on('focus', () => {
+    this.#base.on('focus', () => {
       callback();
     });
   };
 
   loadLocation = (location: string) => {
-    this.browser.loadLocation(location);
+    this.#browser.loadLocation(location);
   };
 
   setBounds = () => {
-    const bounds = this.base.getBounds();
+    const bounds = this.#base.getBounds();
 
-    this.browser.setBounds(bounds, this.navBar.height);
-    this.navBar.setBounds(bounds);
+    this.#browser.setBounds(bounds, this.#navBar.height);
+    this.#navBar.setBounds(bounds);
   };
 }
